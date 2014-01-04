88ce570be24b9892afde775ea5a85a0949621e95
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2014-01-04 10:33:41 -0600

    Added Resource Generator Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freshunderwear::Application.config.secret_key_base = '91cd285be5c69b8782bdba875c96e3d134507275dcc21fe39c2380b9854f674e4d7a7cc8b4b1d19e3d514d254876129d587915e531dae25e85fa91b8aabb4ea6'
