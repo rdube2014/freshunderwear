39e1e5a421be0d1f8aa3ddae8bda72a80dbdd83b
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2014-01-04 16:53:49 -0600

    edited model/table relationships

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,7 +2,9 @@ class Request < ActiveRecord::Base
 
 belongs_to :user
 
-validates :request_text, :presence => true
-validates :travel_to_city, :presence => true
+validates :needed_date, :presence => true
+validates :time_needed, :presence => true
+validates :delivery_city, :presence => true
+validates :delivery_address, :presence => true
 
 end
