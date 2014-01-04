31a238c254b39b4262185ec1b780caf8a99ded3c
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2014-01-04 16:51:08 -0600

    added model/table relationships

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,8 @@
 class Request < ActiveRecord::Base
+
+belongs_to :user
+
+validates :request_text, :presence => true
+validates :travel_to_city, :presence => true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+has_many :requests
+
+def full_name                                                                                                                                                                                     
+  ([first_name, last_name] - ['']).compact.join(' ')                         
+end
+
+validates :first_name, :presence => true
+validates :last_name, :presence => true
+validates :user_name, :presence => true
+
+
+
 end
